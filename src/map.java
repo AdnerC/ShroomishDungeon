d8e6e66850ac089a1f2d065805a6beea359c643d
AdnerC <148999774+AdnerC@users.noreply.github.com>
2024-11-12 11:03:32 -0500

    Update map.java
    figured out what i wanted to do with this class, implemented random encounters and whether or not you are on a special block

--- src/map.java
+++ src/map.java
@@ -1,4 +1,64 @@
-public class map {
+public class Map {
+
+    private final int keyX;
+    private final int keyY;
+    private final int doorX;
+    private final int doorY;
+    private final int armourX;
+    private final int armourY;
+    private final int healthX;
+    private final int healthY;
+
+    public Map(int keyX, int keyY,int doorX,int doorY, int armourX, int armourY, int healthX, int healthY) {
+        this.keyX = keyX;
+        this.keyY = keyY;
+        this.doorX = doorX;
+        this.doorY = doorY;
+        this.armourX = armourX;
+        this.armourY = armourY;
+        this.healthX = healthX;
+        this.healthY = healthY;
+    }
+
+    public int encounterChance(){
+        return (int) (Math.random()*99)+1;
+    }
+
+    public int randomEncounter(){
+        return (int) (Math.random()*2)+1;
+    }
+
+    public boolean isOnKey(int playerX, int playerY){
+        if ((playerX==keyX)&&(playerY==keyY)){
+            return true;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean isOnDoor(int playerX, int playerY){
+        if ((playerX==doorX)&&(playerY==doorY)){
+            return true;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean isOnArmour(int playerX, int playerY){
+        if ((playerX==armourX)&&(playerY==armourY)){
+            return true;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean isOnHealth(int playerX, int playerY){
+        if ((playerX==healthX)&&(playerY==healthY)){
+            return true;
+        }else{
+            return true;
+        }
+    }
 
 
 }
