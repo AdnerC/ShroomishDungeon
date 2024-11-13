1060d1ab5b75a2c9430fb7bd952712aa25d4b716
AdnerC <148999774+AdnerC@users.noreply.github.com>
2024-11-13 10:59:32 -0500

    Update Main.java
    I started working on random encounters, also finished detection for door and key

--- src/Main.java
+++ src/Main.java
@@ -3,93 +3,166 @@ import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        boolean gameWon =false;
+        boolean gameWon = false;
         Player player = new Player(100, 25, 40, 1, 60);
         int playerX = 6;
         int playerY = 6;
         boolean gotKey = false;
+        int keyX;
+        int keyY;
+        int doorX;
+        int doorY;
+        int armourX;
+        int armourY;
+        int healthX;
+        int healthY;
+        double randomness = (((double) (Math.random() * 30) + 85) / 100);
 
         System.out.println("Would you like to play the default map, or have someone create one? (1 for default, 2 for custom)");
         String number = s.nextLine();
         int choice = Integer.parseInt(number);
 
-        if(choice==1){
-            Map gameMap = new Map(6,2,10,4,3,9,2,5);
-        }else {
+        if (choice == 1) {
+            keyX = 6;
+            keyY = 2;
+            doorX = 10;
+            doorY = 4;
+            armourX = 3;
+            armourY = 9;
+            healthX = 2;
+            healthY = 5;
+        } else {
             System.out.println("What would you like the X coordinate of the key to be? (grid is 11x11)");
             String keyXString = s.nextLine();
-            int keyX = Integer.parseInt(keyXString);
+            keyX = Integer.parseInt(keyXString);
             System.out.println("What would you like the Y coordinate of the key to be? (grid is 11x11)");
             String keyYString = s.nextLine();
-            int keyY = Integer.parseInt(keyYString);
+            keyY = Integer.parseInt(keyYString);
 
             System.out.println("What would you like the X coordinate of the door to be? (grid is 11x11)");
             String doorXString = s.nextLine();
-            int doorX = Integer.parseInt(doorXString);
+            doorX = Integer.parseInt(doorXString);
             System.out.println("What would you like the Y coordinate of the key to be? (grid is 11x11)");
             String doorYString = s.nextLine();
-            int doorY = Integer.parseInt(doorYString);
+            doorY = Integer.parseInt(doorYString);
 
             System.out.println("What would you like the X coordinate of the armour to be? (grid is 11x11)");
             String armourXString = s.nextLine();
-            int armourX = Integer.parseInt(armourXString);
+            armourX = Integer.parseInt(armourXString);
             System.out.println("What would you like the Y coordinate of the armour to be? (grid is 11x11)");
             String armourYString = s.nextLine();
-            int armourY = Integer.parseInt(armourYString);
+            armourY = Integer.parseInt(armourYString);
 
             System.out.println("What would you like the X coordinate of the health to be? (grid is 11x11)");
             String healthXString = s.nextLine();
-            int healthX = Integer.parseInt(healthXString);
+            healthX = Integer.parseInt(healthXString);
             System.out.println("What would you like the Y coordinate of the health to be? (grid is 11x11)");
             String healthYString = s.nextLine();
-            int healthY = Integer.parseInt(healthYString);
-
-            Map gameMap = new Map(keyX,keyY,doorX,doorY,armourX,armourY,healthX,healthY);
+            healthY = Integer.parseInt(healthYString);
 
         }
+        Map gameMap = new Map(keyX, keyY, doorX, doorY, armourX, armourY, healthX, healthY);
+        System.out.println(keyX);
 
-        while (!gameWon){
+        while (!gameWon) {
             System.out.println("What direction do you want to move ? (the grid is 11x11) w = up / s = down / a = left / d = right ");
             String direction = s.nextLine();
 
-            if (direction.equals("a")){
-                if(playerX==1){
+            if (direction.equals("a")) {
+                if (playerX == 1) {
                     System.out.println("You can't go that way, theres a wall...");
-                }else {
+                } else {
                     System.out.println("You moved to the left.");
                     playerX--;
                 }
             }
 
-            if (direction.equals("d")){
-                if(playerX==11){
+            if (direction.equals("d")) {
+                if (playerX == 11) {
                     System.out.println("You can't go that way, theres a wall...");
-                }else {
+                } else {
                     System.out.println("You moved to the right.");
                     playerX++;
                 }
             }
 
-            if (direction.equals("s")){
-                if(playerY==1){
+            if (direction.equals("s")) {
+                if (playerY == 1) {
                     System.out.println("You can't go that way, theres a wall...");
-                }else {
+                } else {
                     System.out.println("You moved down.");
                     playerY--;
                 }
             }
 
-            if (direction.equals("w")){
-                if(playerY==11){
+            if (direction.equals("w")) {
+                if (playerY == 11) {
                     System.out.println("You can't go that way, theres a wall...");
-                }else {
+                } else {
                     System.out.println("You moved up.");
-                    playerY--;
+                    playerY++;
                 }
             }
 
-            if ()
-        }
+//            if (gameMap.encounterChance() > 70) {
+//                if (gameMap.randomEncounter() == 1) {
+//                    System.out.println("A small goblin approaches looking for a fight.");
+//                    System.out.println("What do you want to do? (1 fight / 2  run attempt)");
+//                    String fightOption = s.nextLine();
+//                    int combat = Integer.parseInt(number);
+//                    if (combat == 1) {
+//
+//                        double goblinHealth = 100;
+//
+//                        while (goblinHealth > 0) {
+//                            double goblinDmg = 7 * randomness;
+//                            double playerHit = player.getStrong() * randomness;
+//                            System.out.println("You did " + player.dealDamage(playerHit, 5) + " damage!");
+//                            System.out.println("The goblin hit you for " + player.takeDamage(goblinDmg) + "damage!");
+//                            player.changeHealth(-1 * (player.takeDamage(goblinDmg)));
+//                            goblinHealth -= player.dealDamage(playerHit, 5);
+//                        }
+//
+//
+//                    } else {
+//                        if (player.getSpeed() > (40 * randomness)) {
+//                            System.out.println("you managed to escape!");
+//                        }
+//                    }
+//                }
 
+                System.out.println("");
+                System.out.println(playerX);
+                System.out.println(playerY);
+                System.out.println("");
+                System.out.println(gameMap.getKey());
+
+                if ((gameMap.isOnKey(playerX, playerY))) {
+                    gotKey = true;
+                    System.out.println("You got the key!");
+                }
+
+                if (gameMap.isOnDoor(playerX, playerY) && gotKey) {
+                    gameWon = true;
+                }
+
+            }
+            System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⢀⣀⡠⠤⠴⠚⣿⠃\n" +
+                    "⠀⠸⣿⡭⣭⣿⣽⣿⣿⣿⣿⣿⣿⣿⣽⣿⡿⠓⠚⠉⣉⣀⣤⡤⣴⠀⣿⠀\n" +
+                    "⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢰⠞⢩⠀⢻⡏⠀⡏⠀⣿⠄\n" +
+                    "⠀⢠⣟⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢸⠀⢸⠀⢸⡇⠀⠃⠀⣿⠂\n" +
+                    "⠀⢘⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢸⠀⢸⠀⢸⡇⠀⡇⠀⣿⡇\n" +
+                    "⠀⠈⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢸⠀⢸⠀⢸⡇⠀⣷⠀⣿⡇\n" +
+                    "⠀⣠⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢸⠀⢸⠀⢸⡇⠀⣿⣼⣿⡇\n" +
+                    "⠀⡃⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠘⠛⠛⠒⠛⠓⠛⠛⣿⣿⡇\n" +
+                    "⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢰⠦⢠⠀⢤⣤⣤⣄⠋⣿⡇\n" +
+                    "⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢸⠀⢸⠀⢸⡇⠈⣿⠀⣿⡇\n" +
+                    "⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢸⠀⢸⠀⢸⡇⠀⣿⠀⣿⡇\n" +
+                    "⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢸⣄⢸⠠⣼⡇⠀⣿⠀⣿⡇\n" +
+                    "⠀⣸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠉⠉⠀⠛⠚⠯⠿⠀⣿⡇\n" +
+                    "⠠⢿⣿⣷⣶⣶⣶⠶⢶⡶⢶⣶⣶⣶⣶⢿⣶⣤⣄⣀⣀⠀⠀⠀⢨⠀⣿⡇\n" +
+                    "⠀⠀⠀⠈⠀⠐⠒⠒⠀⠀⠀⠘⠁⠈⠀⠀⠀⠀⠉⠉⢛⠉⠑⠒⠠⠤⢿⠇");
+            System.out.println("You escaped the dungeon!");
+
+        }
     }
-}
