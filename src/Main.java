5c87df37aed5a0d9e8f3b0d344df32e8555cdbee
AdnerC <148999774+AdnerC@users.noreply.github.com>
2024-11-19 11:00:36 -0500

    Update Main.java
    figured out how to fix the goblin encounter, and figured out a method for the QTE final boss

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner s = new Scanner(System.in);
         boolean gameWon = false;
         Player player = new Player(100, 25, 40, 1, 60);
@@ -19,6 +21,7 @@ public class Main {
         int healthY;
         double randomness = (((double) (Math.random() * 30) + 85) / 100);
         boolean gotArmour = false;
+
 
         System.out.println("Would you like to play the default map, or have someone create one? (1 for default, 2 for custom)");
         String number = s.nextLine();
@@ -113,36 +116,53 @@ public class Main {
 
                     double goblinHealth = 100;
 
-                    while (goblinHealth > 0 && player.getHealth() > 0) {
-                        System.out.println("What do you want to do? (1 fight / 2 run attempt)");
-                        String fightOption = s.nextLine();
-                        int combat = Integer.parseInt(fightOption);
+                   while (goblinHealth > 0 && player.getHealth() > 0) {
+                       int combat = 0;
+                       String fightOption = "";
 
-                        if (combat == 2) {
-                            double escapeChance = player.getSpeed() * randomness;
-                            double requiredEscapeChance = 40 * randomness;
 
-                            if (escapeChance > requiredEscapeChance) {
-                                System.out.println("You managed to escape!");
-                                return;
-                            } else {
-                                System.out.println("You failed to escape and must fight!");
-                            }
-                        }
+                       boolean optionInt =false;
+                       while (!optionInt){
+                           System.out.println("What do you want to do? (1 fight / 2 run attempt)");
+                           fightOption = s.nextLine();
+                           System.out.println(fightOption);
+                           if (!Objects.equals(fightOption, "1") && !Objects.equals(fightOption, "2")){
+                               System.out.println("Sorry, but that isn't an option!");
+                               System.out.println("What do you want to do? (1 fight / 2 run attempt)");
+                           }else{
+                               optionInt = true;
+                           }
 
-                        if (combat == 1) {
-                            double goblinDmg = 7 * randomness;
-                            double playerHit = player.getStrong() * randomness;
+                       }
 
-                            System.out.println("You did " + player.dealDamage(playerHit, 5) + " damage!");
-                            goblinHealth -= player.dealDamage(playerHit, 5);
+                       combat = Integer.parseInt(fightOption);
 
-                            if (goblinHealth > 0) {
-                                System.out.println("The goblin hit you for " + player.takeDamage(goblinDmg) + " damage!");
-                                player.changeHealth(-1 * player.takeDamage(goblinDmg));
-                            }
-                        }
-                    }
+
+                       if (combat == 2) {
+                           double escapeChance = player.getSpeed() * randomness;
+                           double requiredEscapeChance = 40 * randomness;
+
+                           if (escapeChance > requiredEscapeChance) {
+                               System.out.println("You managed to escape!");
+                               return;
+                           } else {
+                               System.out.println("You failed to escape and must fight!");
+                           }
+                       }
+
+                       if (combat == 1) {
+                           double goblinDmg = 7 * randomness;
+                           double playerHit = player.getStrong() * randomness;
+
+                           System.out.println("You did " + player.dealDamage(playerHit, 5) + " damage!");
+                           goblinHealth -= player.dealDamage(playerHit, 5);
+
+                           if (goblinHealth > 0) {
+                               System.out.println("The goblin hit you for " + player.takeDamage(goblinDmg) + " damage!");
+                               player.changeHealth(-1 * player.takeDamage(goblinDmg));
+                           }
+                       }
+                   }
 
                     if (player.getHealth() <= 0) {
                         System.out.println("The goblin defeated you. Game over.");
@@ -205,18 +225,75 @@ public class Main {
                     "⠀⠀⠀⠈⠀⠐⠒⠒⠀⠀⠀⠘⠁⠈⠀⠀⠀⠀⠉⠉⢛⠉⠑⠒⠠⠤⢿⠇");
             System.out.println("You escaped the dungeon!");
             for (int i = 0; i < 50; ++i) {
-            System.out.println();
+            System.out.println("");
             }
 
-        System.out.println("But then suddenly a disgustingly large appears in the middle of the street, he is all red, and he looks like a weird stubby mushroom.");
+        System.out.println("But then suddenly a disgustingly large monster appears in the middle of the street. He is all red, and he looks like a weird stubby mushroom.");
         System.out.println("");
         System.out.println("He starts running towards you!");
+        for (int i = 0; i < 50; ++i) {
+            System.out.println("");
+        }
 
 
 
 
 
+        System.out.println("QUICK TIME EVENT");
+        System.out.println("");
+        System.out.println("He tries to grab you! Type 'duck' to evade within 3 seconds!");
+
+        // Set up the timer task
+        TimerTask failDuck = new TimerTask() {
+            public void run() {
+                System.out.println("You took too long! The monster grabs you and you lose!");
+                System.exit(0); // Exit the game
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(failDuck, 3000); // Give the user 3 seconds to respond
+
+        // Capture user input
+        String quickTimeEvent = s.nextLine();
+        timer.cancel();
+
+        // Check user input
+        if (quickTimeEvent.equalsIgnoreCase("duck")) {
+            System.out.println("You duck just in time! The monster misses you!");
+        } else {
+            System.out.println("Wrong move! The monster grabs you, and tears a leg off");
+        }
+
+
+
+
+
+
+        System.out.println("He tries to grab you! Type 'roll' to evade within 3 seconds!");
+
+        // Set up the timer task
+        TimerTask failRoll = new TimerTask() {
+            public void run() {
+                System.out.println("You took too long! The monster grabs you and you lose!");
+                System.exit(0); // Exit the game
+            }
+        };
+
+        Timer timer2 = new Timer();
+        timer2.schedule(failRoll, 3000); // Give the user 3 seconds to respond
+
+        // Capture user input
+        String rollTimeEvent = s.nextLine();
+
+        // Check user input
+        if (rollTimeEvent.equalsIgnoreCase("roll")) {
+            System.out.println("You roll just in time! The monster misses you!");
+        } else {
+            System.out.println("Wrong move! The monster bites you, and tears an arm off");
+        }
 
 
     }
-    }
+}
+
