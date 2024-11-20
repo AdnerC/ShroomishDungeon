f9ded1efd0256f7545c004760815e8f8c61208a2
AdnerC <148999774+AdnerC@users.noreply.github.com>
2024-11-20 11:05:26 -0500

    Update and rename Main.java to ShroomishDungeon.java
    I finished the QTE and made certain interfaces better for inputs. Goblin also has rounded and fixed dmg

--- src/ShroomishDungeon.java
+++ src/ShroomishDungeon.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Main {
+public class ShroomishDungeon {
     public static void main(String[] args) throws InterruptedException {
         Scanner s = new Scanner(System.in);
         boolean gameWon = false;
         Player player = new Player(100, 25, 40, 1, 60);
+        boolean mapInt = false;
         int playerX = 6;
         int playerY = 6;
         boolean gotKey = false;
@@ -17,15 +18,24 @@ public class Main {
         int doorY;
         int armourX;
         int armourY;
-        int healthX;
+         int healthX;
         int healthY;
-        double randomness = (((double) (Math.random() * 30) + 85) / 100);
+        double randomness = (((Math.random() * 20) + 95) / 100);
         boolean gotArmour = false;
-
-
+        String number = "";
         System.out.println("Would you like to play the default map, or have someone create one? (1 for default, 2 for custom)");
-        String number = s.nextLine();
+        number = s.nextLine();
+        while(!mapInt){
+            if (!Objects.equals(number, "1") && !Objects.equals(number, "2")) {
+                System.out.println("Sorry, but that isn't an option!");
+                System.out.println("Would you like to play the default map, or have someone create one? (1 for default, 2 for custom)");
+                number = s.nextLine();
+            } else {
+                mapInt = true;
+            }
+        }
         int choice = Integer.parseInt(number);
+
 
         if (choice == 1) {
             keyX = 6;
@@ -70,6 +80,8 @@ public class Main {
         Map gameMap = new Map(keyX, keyY, doorX, doorY, armourX, armourY, healthX, healthY);
 
         while (!gameWon) {
+            System.out.println("Your current location on the 11x11 grid is ("+ playerX+", "+playerY+")");
+            System.out.println();
             System.out.println("What direction do you want to move ? (the grid is 11x11) w = up / s = down / a = left / d = right ");
             String direction = s.nextLine();
 
@@ -116,53 +128,58 @@ public class Main {
 
                     double goblinHealth = 100;
 
-                   while (goblinHealth > 0 && player.getHealth() > 0) {
-                       int combat = 0;
-                       String fightOption = "";
+                    while (goblinHealth > 0 && player.getHealth() > 0) {
+                        int combat = 0;
+                        String fightOption = "";
+
+                        System.out.println("What do you want to do? (1 fight / 2 run attempt)");
+                        fightOption = s.nextLine();
+                        boolean optionInt =false;
+                        while (!optionInt){
+
+                            if (!Objects.equals(fightOption, "1") && !Objects.equals(fightOption, "2")){
+                                System.out.println("Sorry, but that isn't an option!");
+                                System.out.println("What do you want to do? (1 fight / 2 run attempt)");
+                                fightOption = s.nextLine();
+
+                            }else{
+                                optionInt = true;
+                            }
+
+                        }
+
+                        combat = Integer.parseInt(fightOption);
 
 
-                       boolean optionInt =false;
-                       while (!optionInt){
-                           System.out.println("What do you want to do? (1 fight / 2 run attempt)");
-                           fightOption = s.nextLine();
-                           System.out.println(fightOption);
-                           if (!Objects.equals(fightOption, "1") && !Objects.equals(fightOption, "2")){
-                               System.out.println("Sorry, but that isn't an option!");
-                               System.out.println("What do you want to do? (1 fight / 2 run attempt)");
-                           }else{
-                               optionInt = true;
-                           }
+                        if (combat == 2) {
+                            double escapeChance = player.getSpeed() * randomness;
+                            double requiredEscapeChance = 40 * randomness;
 
-                       }
+                            if (escapeChance > requiredEscapeChance) {
+                                System.out.println("You managed to escape!");
+                                return;
+                            } else {
+                                System.out.println("You failed to escape and must fight!");
+                            }
+                        }
 
-                       combat = Integer.parseInt(fightOption);
+                        if (combat == 1) {
+                            double goblinDmg = 60 * randomness;
+                            double playerHit = player.getStrong() * randomness;
 
+                            System.out.println("You did " + (int) player.dealDamage(playerHit, 5) + " damage!");
+                            goblinHealth -= player.dealDamage(playerHit, 5);
 
-                       if (combat == 2) {
-                           double escapeChance = player.getSpeed() * randomness;
-                           double requiredEscapeChance = 40 * randomness;
-
-                           if (escapeChance > requiredEscapeChance) {
-                               System.out.println("You managed to escape!");
-                               return;
-                           } else {
-                               System.out.println("You failed to escape and must fight!");
-                           }
-                       }
-
-                       if (combat == 1) {
-                           double goblinDmg = 7 * randomness;
-                           double playerHit = player.getStrong() * randomness;
-
-                           System.out.println("You did " + player.dealDamage(playerHit, 5) + " damage!");
-                           goblinHealth -= player.dealDamage(playerHit, 5);
-
-                           if (goblinHealth > 0) {
-                               System.out.println("The goblin hit you for " + player.takeDamage(goblinDmg) + " damage!");
-                               player.changeHealth(-1 * player.takeDamage(goblinDmg));
-                           }
-                       }
-                   }
+                            if (goblinHealth > 0) {
+                                if (player.takeDamage(goblinDmg)>0){
+                                    System.out.println("The goblin hit you for " + (int) player.takeDamage(goblinDmg) + " damage!");
+                                    player.changeHealth(-1 * player.takeDamage(goblinDmg));
+                                }else {
+                                    System.out.println("The goblin hit you for no damage");
+                                }
+                            }
+                        }
+                    }
 
                     if (player.getHealth() <= 0) {
                         System.out.println("The goblin defeated you. Game over.");
@@ -225,14 +242,14 @@ public class Main {
                     "⠀⠀⠀⠈⠀⠐⠒⠒⠀⠀⠀⠘⠁⠈⠀⠀⠀⠀⠉⠉⢛⠉⠑⠒⠠⠤⢿⠇");
             System.out.println("You escaped the dungeon!");
             for (int i = 0; i < 50; ++i) {
-            System.out.println("");
+            System.out.println();
             }
 
         System.out.println("But then suddenly a disgustingly large monster appears in the middle of the street. He is all red, and he looks like a weird stubby mushroom.");
-        System.out.println("");
+        System.out.println();
         System.out.println("He starts running towards you!");
         for (int i = 0; i < 50; ++i) {
-            System.out.println("");
+            System.out.println();
         }
 
 
@@ -240,19 +257,19 @@ public class Main {
 
 
         System.out.println("QUICK TIME EVENT");
-        System.out.println("");
+        System.out.println();
         System.out.println("He tries to grab you! Type 'duck' to evade within 3 seconds!");
 
         // Set up the timer task
         TimerTask failDuck = new TimerTask() {
             public void run() {
                 System.out.println("You took too long! The monster grabs you and you lose!");
-                System.exit(0); // Exit the game
+                System.exit(0);
             }
         };
 
         Timer timer = new Timer();
-        timer.schedule(failDuck, 3000); // Give the user 3 seconds to respond
+        timer.schedule(failDuck, 2500); // Give the user 3 seconds to respond
 
         // Capture user input
         String quickTimeEvent = s.nextLine();
@@ -266,8 +283,7 @@ public class Main {
         }
 
 
-
-
+        Thread.sleep(200);
 
 
         System.out.println("He tries to grab you! Type 'roll' to evade within 3 seconds!");
@@ -281,16 +297,45 @@ public class Main {
         };
 
         Timer timer2 = new Timer();
-        timer2.schedule(failRoll, 3000); // Give the user 3 seconds to respond
+        timer2.schedule(failRoll, 2500); // Give the user 3 seconds to respond
 
         // Capture user input
         String rollTimeEvent = s.nextLine();
+        timer2.cancel();
 
         // Check user input
         if (rollTimeEvent.equalsIgnoreCase("roll")) {
             System.out.println("You roll just in time! The monster misses you!");
         } else {
             System.out.println("Wrong move! The monster bites you, and tears an arm off");
+        }
+
+
+
+
+
+        System.out.println("He starts to swell, it looks like hes going to explode! Type 'sprint' to evade within 3 seconds!");
+
+        // Set up the timer task
+        TimerTask failSprint = new TimerTask() {
+            public void run() {
+                System.out.println("You took too long! The monster explodes and leaves corrosive acid all over you!");
+                System.exit(0); // Exit the game
+            }
+        };
+
+        Timer timer3 = new Timer();
+        timer3.schedule(failSprint, 3000); // Give the user 3 seconds to respond
+
+        // Capture user input
+        String sprintTimeEvent = s.nextLine();
+        timer3.cancel();
+
+        // Check user input
+        if (sprintTimeEvent.equalsIgnoreCase("sprint")) {
+            System.out.println("You sprint just in time! The monster monster explodes, but you managed to get awawy!!");
+        } else {
+            System.out.println("Wrong move! You try to run, but trip! ");
         }
 
 
